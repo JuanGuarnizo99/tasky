c52174fe56a69067b800fd4f79d7bf3b52b8d1f6
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-07-08 17:38:43 -0400

    Update Banner.js
    fixed button container issue

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import Modal from "./Modal";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 
-function Banner({getData}) {
+function Banner({getData, authToken}) {
   const [cookies, setCookies, removeCookies] = useCookies(null);
   //sign out function
   const signOut = () => {
@@ -17,11 +17,12 @@ function Banner({getData}) {
     return (
       <div className="banner">
         <p className="tasky">Tasky</p>
-        <div className="button-container">
+        {authToken? <div className="button-container">
           <button className="create" onClick={() => setShowModal(true)}>New</button>
           <button className="signout" onClick={signOut}>Sign Out</button>
           {showModal && <Modal mode={"create"} setShowModal={setShowModal} getData={getData}/>}
-        </div>
+        </div>: null}
+        
       </div>
     );
   }
