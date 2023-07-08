827d9d3f1c94f5b7a5f78469c82ba058d0934d8d
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-07-08 17:22:38 -0400

    Update Banner.js
    Moved button container to live inside this component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,27 @@
-function Banner() {
-    
+import Modal from "./Modal";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+
+function Banner({getData}) {
+  const [cookies, setCookies, removeCookies] = useCookies(null);
+  //sign out function
+  const signOut = () => {
+    removeCookies('Email');
+    removeCookies('AuthToken');
+    window.location.reload();
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
   
     return (
       <div className="banner">
         <p className="tasky">Tasky</p>
+        <div className="button-container">
+          <button className="create" onClick={() => setShowModal(true)}>New</button>
+          <button className="signout" onClick={signOut}>Sign Out</button>
+          {showModal && <Modal mode={"create"} setShowModal={setShowModal} getData={getData}/>}
+        </div>
       </div>
     );
   }
