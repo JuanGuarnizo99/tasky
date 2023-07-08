7d41c8e002756a1ab3da767716b0ba845e7212fc
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-07-08 17:23:40 -0400

    Update ListHeader.js
    Removed unnecessary code

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -1,19 +1,4 @@
-import Modal from "./Modal";
-import { useState } from "react";
-import { useCookies } from "react-cookie";
-
-function ListHeader({listName, getData}) {
-  const [cookies, setCookies, removeCookies] = useCookies(null);
-  //sign out function
-  const signOut = () => {
-    removeCookies('Email');
-    removeCookies('AuthToken');
-    window.location.reload();
-  };
-
-  const [showModal, setShowModal] = useState(false);
-
-
+function ListHeader({listName}) {
   return (
     <div className="list-header">
       <h1>{listName}</h1>
