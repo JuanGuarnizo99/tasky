b1d10600cc9bf3133cfe6d2690d67b331c17cf86
Juan Guarnizo <88952365+JuanGuarnizo99@users.noreply.github.com>
2023-07-08 17:22:04 -0400

    Update List Header
    Removed button container from this component

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -17,12 +17,6 @@ function ListHeader({listName, getData}) {
   return (
     <div className="list-header">
       <h1>{listName}</h1>
-      <div className="button-container">
-        <button className="create" onClick={() => setShowModal(true)}>New</button>
-        <button className="signout" onClick={signOut}>Sign Out</button>
-      </div>
-      {/* If I click on the 'create' button, it shows the modal asking for the new task's data */}
-      {showModal && <Modal mode={"create"} setShowModal={setShowModal} getData={getData}/>}
     </div>
   );
 }
